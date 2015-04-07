5d45a0449eac91e2a6c03ff8361c8f4b8c8bf35e
TigerJ <jimmy.briggs.code@gmail.com>
2015-04-06 23:09:32 -0400

    finished, time for coffee

--- lib/lita/handlers/starbucks.rb
+++ lib/lita/handlers/starbucks.rb
@@ -1,8 +1,14 @@
 module Lita
   module Handlers
     class Starbucks < Handler
+      route(/^starbucks all/i, :Starbucks, command: true, help: {
+        "starbucks all" => "informs @all members of the room you intend to go to starbucks"
+      })
+      def Starbucks(response)
+        response.reply "https://s3.amazonaws.com/devbriggs.com/starbucks/#{rand(1..3)}.jpg @all Starbucks Time"
+      end
     end
-
+    
     Lita.register_handler(Starbucks)
   end
 end
